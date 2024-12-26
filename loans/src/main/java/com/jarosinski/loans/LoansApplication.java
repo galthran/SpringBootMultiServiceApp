6242559acdfdd003f04eef1d080951d73d451d49
Galthran <rafal.jarosinski@gmail.com>
2024-12-26 19:28:33 +0100

    Implementing loans module

--- loans/src/main/java/com/jarosinski/loans/LoansApplication.java
+++ loans/src/main/java/com/jarosinski/loans/LoansApplication.java
@@ -1,9 +1,31 @@
 package com.jarosinski.loans;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Loans microservice REST API Documentation",
+        description = "EazyBank Loans microservice REST API Documentation",
+        version = "v1",
+        contact = @Contact(
+            name = "Madan Reddy",
+            email = "tutor@eazybytes.com",
+            url = "https://www.eazybytes.com"
+        ),
+        license = @License(
+            name = "Apache 2.0",
+            url = "https://www.eazybytes.com"
+        )
+    )
+)
 public class LoansApplication {
 
     public static void main(String[] args) {
